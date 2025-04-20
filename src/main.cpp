f9c57dc1f3cc4eec470c70bd9f97e2dc3ef79a2c
MaybeAshLately <maja.guzewska2001@gmail.com>
2025-04-20 13:51:19 +0200

    Basic program structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,93 @@
 #include <Arduino.h>
+#define photoresistorPin A5
+#define redPin 9
+#define greenPin 10
+#define bluePin 11
+#define pirPin 2
 
-// put function declarations here:
-int myFunction(int, int);
+int redLevel=0;
+int greenLevel=0;
+int blueLevel=0;
+
+int criticalLightLevel=0;
+
+bool isOn=false;
+bool photoresistorDetectionOn=false;
+bool pirDetectionOn=false;
+bool tvModeOn=false;
+
+void playTvMode();
+void photoresistorModeOn();
+void lightsOn();
+void lightsOff();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(redPin,OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+  pinMode(pirPin, INPUT);
+
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if(isOn)
+  {
+    lightsOn();
+  }
+  else if(photoresistorDetectionOn)
+  {
+    photoresistorModeOn();
+  }
+  else if(pirDetectionOn)
+  {
+    // TODO
+  }
+  else if(tvModeOn)
+  {
+    playTvMode();
+  }
+  else
+  {
+    lightsOff();
+  }
+  
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void serialEvent()
+{
+  //TODO handle incoming message
+}
+
+
+void playTvMode()
+{
+
+}
+
+void photoresistorModeOn()
+{
+  int lightLevel=analogRead(photoresistorPin);
+  if(lightLevel<criticalLightLevel) lightsOn();
+}
+
+void lightsOn()
+{
+  analogWrite(redPin,redLevel);
+  analogWrite(greenPin,greenLevel);
+  analogWrite(bluePin,blueLevel);
+}
+
+void lightsOff()
+{
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,LOW);
 }
